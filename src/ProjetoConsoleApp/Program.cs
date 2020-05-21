f56d762158ec8d0302ea8bca929d71fbb3d0eb9e
alexsandrox <alexandrade.1979@gmail.com>
2020-05-21 01:52:12 -0300

    Teste de funcionamento 4

--- src/ProjetoConsoleApp/Program.cs
+++ src/ProjetoConsoleApp/Program.cs
@@ -10,7 +10,7 @@ namespace ProjetoConsoleApp
             var operadoraDeCartao = new OperadoraDeCartaoCielo();
             var impressora = new ImpressoraEpson();
 
-            var cartao = new Cartao("Alexsandro V A Andrade", "3456");
+            var cartao = new Cartao("GLADYS M R T DE PAULA", "7432");
             var compra = new Compra(1899.99, "Playstation 4 PRO", "Alexsandro Andrade");
             var checkout = new Checkout(operadoraDeCartao, impressora);
             checkout.FecharCompra(compra, cartao);
